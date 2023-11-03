8921e0e8a0e95908003b1710fde0baf29179ee33
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-11-03 18:21:08 -0500

    main commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const url = "https://api.openweathermap.org/data/2.5/forecast?q=Atlanta,US&appid=d296f93db7d79c41ef8a6d563f1b4432"
+
+//function fetchResults() {
+   // fetch(url)
+   // .then(response)
+   // .then()
+  //  .catch
+//}
+//fetchResults();

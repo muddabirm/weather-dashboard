725fe0fb8296a98790aabcc72e098ae364295573
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-11-15 11:41:29 -0600

    added fetch and the right url

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const url = "https://api.openweathermap.org/data/2.5/forecast?q=Atlanta,US&appid=d296f93db7d79c41ef8a6d563f1b4432"
+const btn = document.getElementById("button")
+
+btn.addEventListener("click", function(){
+  const searchResult = document.querySelector("#search").value
+
+  const url = "https://api.openweathermap.org/data/2.5/forecast?q="+ searchResult +",US&appid=d296f93db7d79c41ef8a6d563f1b4432"
+  fetch(url)
+  console.log(url)
+  console.log(searchResult)
+})
+
+
+
 
 //function fetchResults() {
    // fetch(url)

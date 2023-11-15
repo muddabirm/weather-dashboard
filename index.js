666f7a1b63fa03cc73c48b4a725ec55b89bafbca
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-11-15 14:37:49 -0600

    fixed css

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let name_ = document.querySelector("#citySearch")
 let temp_ = document.querySelector("#tempSearch")
 let wind_ = document.querySelector("#windSearch")
 let humidity_ = document.querySelector("#humiditySearch")
+let saved = document.querySelector("#saved")
 
 btn.addEventListener("click", function(){
   const searchResult = document.querySelector("#search").value
-
+  //trying to impliment the local storage
   const url = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q="+ searchResult +",US&appid=d296f93db7d79c41ef8a6d563f1b4432"
   fetch(url)
   .then(function(response){
@@ -15,14 +16,14 @@ btn.addEventListener("click", function(){
   .then(function(data){
     console.log(data)
     name_.innerHTML = data.city.name
-    //wind_.innerHTML = data.main.humidity
-    //humidity_.innerHTML= data.main.humidity
+    wind_.innerHTML = "speed: " + data.list[0].wind.speed
+    humidity_.innerHTML= "humidity: " + data.list[0].main.humidity + " %"
     //figured out you had to use the array it was in to get the right properties...
-    temp_.innerHTML= data.list[0].main.temp
+    temp_.innerHTML= "temp: " + data.list[0].main.temp + " F"
     
   })
-  console.log(url)
-  console.log(searchResult)
+  localStorage.setItem("City", searchResult)
+   saved.innerHTML = localStorage.getItem("City")
 })
 
 
@@ -36,6 +37,4 @@ btn.addEventListener("click", function(){
    // fetch(url)
    // .then(response)
    // .then()
-  //  .catch
-//}
-//fetchResults();
+ 
